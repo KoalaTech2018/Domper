d064066b38da46366e1232542a7ecd0b2c10aab0
Wong <galaxy.wong@ap.win.int.kn>
2018-09-20 17:01:51 +0800

    fix object list on detail page

--- src/pages/maid/detail.ts
+++ src/pages/maid/detail.ts
@@ -1,25 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { Platform, NavParams, ViewController } from 'ionic-angular';
+import { Injectable, Pipe, PipeTransform } from "@angular/core";
 // import { ProjectDto } from './projectDto';
 
 @Component({
-    selector: 'page-maid',
-    templateUrl: 'detail.html'
-  })
-  
-export class ModalContentPage {
-    maid;
-    constructor(
-        public platform: Platform,
-        public params: NavParams,
-        public viewCtrl: ViewController
-    ) {
-        console.log('I am created');
-        this.maid = this.params.get('obj');
-    }
+  selector: "page-maid",
+  templateUrl: "detail.html"
+})
+export class ModalContentPage implements OnInit {
 
-    dismiss() {
-        this.viewCtrl.dismiss();
-    }
+  object: { [key: number]: string } =
+    { 2: 'Angular keyvalue Pipe', 1: 'Angular ngFor' };
+
+  public arrayOfKeys;
+  maid;
+
+  constructor(
+    public platform: Platform,
+    public params: NavParams,
+    public viewCtrl: ViewController
+  ) {
+    console.log("I am created");
+    this.maid = this.params.get("obj");
+    console.log(this.maid.working_exps);
+    this.arrayOfKeys = this.maid.working_exps;
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
+  ngOnInit() {}
+  
 }
+
+
+
   
