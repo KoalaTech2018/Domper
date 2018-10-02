5723324b10fdd236fdc259dad16a4821e7c80542
Wong <galaxy.wong@ap.win.int.kn>
2018-10-02 18:16:48 +0800

    update setting page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,5 +33,4 @@ export class MyApp {
       splashScreen.hide();
     });
   }
-
 }
--- src/pages/setting/setting.ts
+++ src/pages/setting/setting.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'page-setting',
-  templateUrl: 'setting.html'
+  selector: "page-setting",
+  templateUrl: "setting.html"
 })
 export class SettingPage {
+  language: any;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController,
+    public translate: TranslateService
+  ) {
+    this.language = navigator.language;
   }
 
+  changeLanguage() {
+    console.log(this.language);
+    this.translate.use(this.language);
+  }
 }
