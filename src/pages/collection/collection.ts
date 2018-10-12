547f51f209524f82510cf06d216f362a92ce8b53
KoalaTech2018 <KoalaTech2018@gmail.com>
2018-10-13 00:16:23 +0800

    add create function on add to collection button

--- src/pages/collection/collection.ts
+++ src/pages/collection/collection.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as firebase from "firebase/app";
+import { AngularFireAuth } from "angularfire2/auth";
+import { Observable } from "rxjs/Observable";
 
 @Component({
-  selector: 'page-collection',
-  templateUrl: 'collection.html'
+  selector: "page-collection",
+  templateUrl: "collection.html"
 })
 export class CollectionPage {
+  user: Observable<firebase.User>;
+  userId;
 
-  constructor(public navCtrl: NavController) {
+  constructor(private afAuth: AngularFireAuth, 
+    public navCtrl: NavController) {
 
+    this.user = this.afAuth.authState;
+    this.afAuth.auth.onAuthStateChanged(user => {
+      if (user) {
+        this.userId = user.uid;
+        console.log(this.userId);
+      } else {
+        this.userId = null;
+      }
+    });
   }
-
 }
